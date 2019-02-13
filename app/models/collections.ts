45667cd98477f899f194dfd08d34f96d77995f72
vadamk <volodymyr.kravch@gmail.com>
2019-02-13 13:39:55 +0200

    fix: fix collections list

--- app/models/collections.ts
+++ app/models/collections.ts
@@ -1,3 +1,5 @@
 export const REGISTRATION_REQUESTS = 'registration-requests';
 export const USERS = 'users';
-export const CHANNELS = 'channels';
+export const PERSONS = 'persons';
+export const TELEGRAM_OUTLETS = 'telegram-outlets';
+export const VIBER_OUTLETS = 'viber-groups-outlets';
