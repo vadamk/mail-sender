4630af668a42377a706b06083bba2a7aee21acfe
vadamk <volodymyr.kravch@gmail.com>
2019-02-13 12:19:21 +0200

    fix: login validation

--- app/controllers/auth/login.ts
+++ app/controllers/auth/login.ts
@@ -11,13 +11,17 @@ export const loginRequest = async (ctx: Context) => {
   const reqBody: LoginRequest = ctx.request.body;
 
   const user = await ctx.db.collection(USERS).findOne({ 'email': reqBody.email });
-  const verified = await compare(reqBody.password, user.password);
 
-  if (user && verified) {
-    ctx.status = 200;
-    ctx.body = { token: jwtSing(user) };
-  } else {
-    ctx.status = 400;
-    ctx.body = { message: 'Sorry, user doen\'t exists or password is wrong.' };
+  if (!!user) {
+
+    const verified = await compare(reqBody.password, user.password);
+
+    if (verified) {
+      ctx.status = 200;
+      ctx.body = { token: jwtSing(user) };
+    } else {
+      ctx.status = 400;
+      ctx.body = { message: 'Sorry, user doen\'t exists or password is wrong.' };
+    }
   }
 };
